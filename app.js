095a12d8e0d9e75f6c10d33f62460f1fe37b75a6
John Malapit <johncmalapit@gmail.com>
2022-10-29 20:57:34 -0400

    complete exercise 11.6 - fix eslint errors

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-const express = require("express");
-const app = express();
+const express = require('express')
+const app = express()
 
 // Heroku dynamically sets a port
-const PORT = process.env.PORT || 2121;
+const PORT = process.env.PORT || 2121
 
-app.use(express.static("dist"));
+app.use(express.static('dist'))
 
 app.listen(PORT, () => {
-  console.log("server started on port 2121");
-});
+  console.log('server started on port 2121')
+})
