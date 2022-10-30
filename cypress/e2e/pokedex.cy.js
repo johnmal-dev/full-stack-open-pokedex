d7e0b54462c097ebbc6ab8c7b5d3784a7346b385
John Malapit <johncmalapit@gmail.com>
2022-10-29 22:16:51 -0400

    complete exercise 11.9 - implement cypress tests

--- cypress/e2e/pokedex.cy.js
+++ cypress/e2e/pokedex.cy.js
@@ -5,5 +5,10 @@ describe('Pokedex', () => {
     cy.contains(
       'Pokémon and Pokémon character names are trademarks of Nintendo.'
     )
+  }),
+  it('pokemon page can be navigated to', () => {
+    cy.visit('http://localhost:2121')
+    cy.contains('ivysaur').click()
+    cy.contains('chlorophyll')
   })
 })
